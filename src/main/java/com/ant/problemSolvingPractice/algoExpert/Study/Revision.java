23931e1f491adc6d0256adede48ec392505fbc78
Mohamed Sayed Khalil <mohamedsayed194@hotmail.com>
2025-10-15 08:12:47 +0300

    Update Revision.java

--- src/main/java/com/ant/problemSolvingPractice/algoExpert/Study/Revision.java
+++ src/main/java/com/ant/problemSolvingPractice/algoExpert/Study/Revision.java
@@ -1,45 +1,100 @@
 package com.ant.problemSolvingPractice.algoExpert.Study;
 
+
+import com.ant.datastrucutreandalog.metwally.Algorithm.GraphDepthFirstSearch;
+
 import java.util.*;
 
 public class Revision {
     public static void main(String[] args) {
-        int[] num = {1, 5, 4, 2, 9, 9, 9};
-        int k = 3;
-        System.out.println(maximumSubarraySum(num, k));
+        List<String> nodes = List.of("A", "B", "C", "D", "E", "F", "G", "H", "I");
+        Graph g = new Graph(nodes);
+
+        g.addEdges(0, List.of(1, 2));
+        g.addEdges(1, List.of(0, 3, 4));
+        g.addEdges(2, List.of(0, 3, 5));
+        g.addEdges(3, List.of(1, 2, 4));
+        g.addEdges(4, List.of(1, 5));
+        g.addEdges(5, List.of(2, 3, 4, 7));
+        g.addEdges(6, List.of(7, 8));
+        g.addEdges(7, List.of(5, 6, 8));
+        g.addEdges(8, List.of(6, 7));
+
+        System.out.println("DFS traversal:");
+        g.dfs();
     }
 
-    public static long maximumSubarraySum(int[] nums, int k) {
-        long maxSum = 0;
-        long currentSum = 0;
-        Map<Integer, Integer> elementsCount = new HashMap<>();
-        // get first 3 elements
-        for (int i = 0; i < k; i++) {
-            elementsCount.put(nums[i], elementsCount.getOrDefault(nums[i], 0) + 1);
-            currentSum += nums[i];
-        }
+//    A -> B
+//    B -> D
+//    D -> C
+//    C -> F
+//    F -> E
+//    F -> H
+//    H -> G
+//    G -> I
+    static class Graph {
+        Vertex[] vertices;
+        int lastIndex = 0;
 
-        if (elementsCount.size() == k) {
-            maxSum = currentSum;
-        }
-
-        for (int i = k; i < nums.length; i++) {
-            // remove element
-            int removeElement = nums[i - k];
-            currentSum -= removeElement;
-            elementsCount.put(removeElement, elementsCount.get(removeElement) - 1);
-            if (elementsCount.get(removeElement) == 0) {
-                elementsCount.remove(removeElement);
-            }
-            int addElement = nums[i];
-            currentSum += addElement;
-            elementsCount.put(addElement, elementsCount.getOrDefault(addElement, 0) + 1);
-
-            if (elementsCount.size() == k) {
-                maxSum = Math.max(maxSum, currentSum);
+        public Graph(List<String> nodes) {
+            vertices = new Vertex[nodes.size()];
+            for (String d : nodes) {
+                Vertex v = new Vertex(d, false);
+                vertices[lastIndex] = v;
+                lastIndex++;
             }
         }
 
-        return maxSum;
+        void addEdges(int vertexIndex, List<Integer> targetsIndex) {
+            Vertex target = this.vertices[vertexIndex];
+            target.edges = new Edge[targetsIndex.size()];
+            for (int i = 0; i < targetsIndex.size(); i++) {
+                Edge edge = new Edge(target, this.vertices[targetsIndex.get(i)]);
+                target.edges[i] = edge;
+            }
+        }
+
+        void dfs() {
+            dfs(this.vertices[0]);
+        }
+
+        void dfs(Vertex current) {
+            current.isVisited = true;
+            Edge[] destination = current.edges;
+            for (Edge e : destination) {
+                if (!e.destination.isVisited) {
+                    System.out.println(current.label + " -> " + e.destination.label);
+                    dfs(e.destination);
+                }
+            }
+        }
+    }
+
+    static class Vertex {
+        String label;
+        boolean isVisited;
+        Edge[] edges;
+
+        public Vertex(String label, boolean isVisited) {
+            this.isVisited = isVisited;
+            this.label = label;
+        }
+    }
+
+    static class Edge {
+        Vertex source;
+        Vertex destination;
+        double weight;
+
+        public Edge(Vertex source, Vertex destination) {
+            this.destination = destination;
+            this.source = source;
+        }
+
+        public Edge(Vertex source, Vertex destination, double weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
     }
 }
